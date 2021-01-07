ff0116b9d1bf9b6172f8edfdc2ed32c76a6c247f
oweyteodosio <74039619+oweyteodosio@users.noreply.github.com>
2021-01-07 22:49:28 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask,redirect
 from flask import render_template
 from flask import request
+from flask import session
+from flask import flash
 import database as db
+import authentication
+import logging
+
 
 app = Flask(__name__)
+
+# Set the secret key to some random bytes.
+# Keep this really secret!
+app.secret_key = b's@g@d@c0ff33!'
+
+
+logging.basicConfig(level=logging.DEBUG)
+app.logger.setLevel(logging.INFO)
+
 
 @app.route('/')
 def index():
@@ -34,3 +48,79 @@ def branchdetails():
 @app.route('/aboutus')
 def aboutus():
     return render_template('aboutus.html', page="About Us")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/auth', methods = ['POST'])
+def auth():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    is_successful, user = authentication.login(username, password)
+    app.logger.info('%s', is_successful)
+    if(is_successful):
+        session["user"] = user
+
+        return redirect('/')
+    else:
+        flash("Invalid username or password. Try again.")
+        return redirect('/login')
+
+
+@app.route('/logout')
+def logout():
+    session.pop("user",None)
+    session.pop("cart",None)
+    return redirect('/')
+
+
+@app.route('/addtocart')
+def addtocart():
+    code = request.args.get('code', '')
+    product = db.get_product(int(code))
+    item=dict()
+    # A click to add a product translates to a
+    # quantity of 1 for now
+
+    item["code"] = code
+    item["qty"] = 1
+    item["name"] = product["name"]
+    item["subtotal"] = product["price"]*item["qty"]
+
+    if(session.get("cart") is None):
+        session["cart"]={}
+
+    cart = session["cart"]
+    cart[code]=item
+    session["cart"]=cart
+    return redirect('/cart')
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
+@app.route('/formsubmission', methods = ['POST'])
+def form_submission():
+    code = request.form.getlist("code")
+    qty = request.form.getlist("qty")
+    cart = session["cart"]
+
+
+    for i in range(len(code)):
+        product = db.get_product(int(code[i]))
+        cart[code[i]]["qty"] = int(qty[i])
+        cart[code[i]]["subtotal"] = int(qty[i]) * product["price"]
+    session["cart"]=cart
+
+    return redirect('/cart')
+
+@app.route('/deleteitem')
+def delete_item():
+    code = request.args.get('code', '')
+    cart = session["cart"]
+    cart.pop(code, None)
+    session["cart"]=cart
+
+    return redirect('/cart')
--- database.py
+++ database.py
@@ -45,3 +45,33 @@ def get_branches():
         branch_list.append(branch)
 
     return branch_list
+    
+users = {
+    "chums@example.com":{"password":"Ch@ng3m3!",
+                         "first_name":"Matthew",
+                         "last_name":"Uy"},
+    "joben@example.com":{"password":"Ch@ng3m3!",
+                         "first_name":"Joben",
+                         "last_name":"Ilagan"},
+    "bong@example.com":{"password":"Ch@ng3m3!",
+                        "first_name":"Bong",
+                        "last_name":"Olpoc"},
+    "joaqs@example.com":{"password":"Ch@ng3m3!",
+                         "first_name":"Joaqs",
+                         "last_name":"Gonzales"},
+    "gihoe@example.com":{"password":"Ch@ng3m3!",
+                         "first_name":"Gio",
+                         "last_name":"Hernandez"},
+    "vic@example.com":{"password":"Ch@ng3m3!",
+                       "first_name":"Vic",
+                       "last_name":"Reventar"},
+    "joe@example.com":{"password":"Ch@ng3m3!",
+                       "first_name":"Joe",
+                       "last_name":"Ilagan"},
+}
+
+def get_user(username):
+    try:
+       return users[username]
+    except KeyError:
+       return None
